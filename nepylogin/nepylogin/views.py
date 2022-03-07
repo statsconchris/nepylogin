05bd90cd4823f95fdb4495a8b9e221d1bfb071b9
statsconchris <statsconchris@gmail.com>
2022-03-07 01:02:50 +0100

    login_2fa-v5

--- nepylogin/nepylogin/views.py
+++ nepylogin/nepylogin/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LogoutView
 
 
 def home(request):
-    return render(request,'home.html')
+    if request.user.is_verified():
+        return render(request,'home.html')
+    else:
+        return redirect("two_factor:setup")
 
 
 def logout(request):
